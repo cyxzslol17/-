8e292ae85f1ab04c20b5ee907fb0988975cc16a6
cyxzslol17 <98207378+cyxzslol17@users.noreply.github.com>
2022-01-23 20:08:50 +0800

    Add files via upload

---
+++ 陈三/源.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//#define宏常量
+#define chenyuxiang "戴拿"
+
+int main() {
+
+	cout << "陈玉祥是" << chenyuxiang << "奥特曼" << endl;
+
+
+
+	const int month = 12;
+	 
+	cout << "一年总共有： " << month << "个月份" << endl;
+	system("pause");
+
+	return 0;
+}
---
+++ 陈二/帅.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	int chenyuxiang = 18;
+
+	cout << "chenyuxiang =" << chenyuxiang << endl;
+
+	system("pause");
+
+
+	"return"; 0;
+}
