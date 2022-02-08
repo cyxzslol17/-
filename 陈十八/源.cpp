42018f234fa6ebc81619d44b54fd60f2df908524
cyxzslol17 <98207378+cyxzslol17@users.noreply.github.com>
2022-02-09 01:49:21 +0800

    Add files via upload

---
+++ 陈十八/源.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	
+	cout << "1 小梦最美" << endl;
+	
+	cout << "2 小梦最美" << endl;
+	goto CYX;
+	cout << "3 小梦最美" << endl;
+
+	cout << "4 小梦最美" << endl;
+	
+	cout << "5 小梦最美" << endl;
+	CYX:
+	
+	cout << "6 小梦最美" << endl;
+
+	
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+
+}
