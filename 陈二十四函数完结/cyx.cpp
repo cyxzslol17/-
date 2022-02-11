97dc8551c3ea8da81ed91e98fc656a4519a0675b
cyxzslol17 <98207378+cyxzslol17@users.noreply.github.com>
2022-02-11 19:39:52 +0800

    Add files via upload

---
+++ 陈二十四函数完结/cyx.cpp
@@ -0,0 +1,15 @@
+#include "cyx.h"
+
+void cyx(int a, int b)
+{
+
+	int temp = a;
+	a = b;
+	b = temp;
+
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+
+}
---
+++ 陈二十四函数完结/cyx.h
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+
+
+void cyx(int a, int b);
